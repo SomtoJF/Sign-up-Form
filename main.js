bee4513b40315a20ef49fe5f99b2b466f08f2e84
Somtochukwu Francis <somtofrancis5@gmail.com>
2022-07-11 01:22:25 -0800

    Remove console.log statement in line 5

--- main.js
+++ main.js
@@ -2,7 +2,6 @@
 let canvas = document.getElementById('typewriter');
 let words = 'Not an actual Service.';
 words = words.split('');
-console.log(words);
 let i = 0;
 function typewriter(){
     if(i < words.length){
