32dae46f4c7dd17e71011e05a0aacbf5761bb2c0
Somtochukwu Francis <somtofrancis5@gmail.com>
2022-07-10 17:15:16 -0800

    Validate password and confirm password in Javascript

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//TYPEWRITER EFFECT
 let canvas = document.getElementById('typewriter');
 let words = 'Not an actual Service.';
 words = words.split('');
@@ -11,3 +12,20 @@ function typewriter(){
     };
 };
 typewriter();
+
+// PASSWORD CHECK
+let password = document.getElementById('password');
+let confirmPassword = document.getElementById('confirm-password');
+
+function passwordCheck() {
+    if(password.value == confirmPassword.value){
+        password.style.border = 'solid green';
+        confirmPassword.style.border = 'solid green';
+    }
+    else{
+        password.style.border = 'solid red';
+        confirmPassword.style.border = 'solid red';
+    }
+}
+password.addEventListener('keyup', passwordCheck);
+confirmPassword.addEventListener('keyup', passwordCheck);
